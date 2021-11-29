ba7cedd20c2688e2faa029391462f3ab321d5f46
Ravi Srinivasan <rsriniva@redhat.com>
2021-11-29 14:42:40 +0530

    fixed broken CI job

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { By } from '@angular/platform-browser'
 import { injectSpy } from 'angular-unit-test-helper'
 import { of } from 'rxjs'
